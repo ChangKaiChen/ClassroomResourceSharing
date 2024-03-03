270f54fc215675ce8f3985cca9b12914e3319106
cck <2385966053@qq.com>
2024-03-03 20:24:36 +0800

    增加注册页面

---
+++ blueprints/users/register.py
@@ -0,0 +1,13 @@
+from . import user_bp
+from flask import request, render_template, jsonify
+
+
+@user_bp.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    else:
+        try:
+            return jsonify({'success': True, 'message': '注册成功'}), 200, {'ContentType': 'application/json'}
+        except Exception as e:
+            return jsonify({'success': False, 'message': '注册失败'}), 200, {'ContentType': 'application/json'}
